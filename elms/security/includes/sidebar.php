1afad853e0305f92707a3de35b913f8add45f69b
eniayomi <oluwaseyieniayomi@gmail.com>
2019-06-23 10:57:51 +0100

    first push

---
+++ elms/security/includes/sidebar.php
@@ -0,0 +1,34 @@
+     <aside id="slide-out" class="side-nav white fixed">
+                <div class="side-nav-wrapper">
+                    <div class="sidebar-profile">
+                        <div class="sidebar-profile-image">
+                            <img src="../assets/images/profile-image.png" class="circle" alt="">
+                        </div>
+                        <div class="sidebar-profile-info">
+                       
+                                <p>Security</p>
+
+                         
+                        </div>
+                    </div>
+            
+                <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
+                    <li class="no-padding"><a class="waves-effect waves-grey" href="dashboard.php"><i class="material-icons">settings_input_svideo</i>Dashboard</a></li>
+                    
+
+
+
+                        <li class="no-padding">
+                                <a class="waves-effect waves-grey" href="logout.php"><i class="material-icons">exit_to_app</i>Sign Out</a>
+                            </li>  
+                 
+
+                 
+              
+                </ul>
+                   <div class="footer">
+                    <p class="copyright">Oluwaseyi Oluwapelumi ©</p>
+                
+                </div>
+                </div>
+            </aside>
